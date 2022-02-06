e0a3eb2fd000312f599ef9fa23d6484eb59332bf
Odumz <odumz20@gmail.com>
2022-02-06 01:52:50 +0100

    starter files updated

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,12 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+import Navbar from '../components/Navbar.vue'
+import Home from '../components/Index.vue'
+</script>
+
+
 <template>
-    <div class="text-5xl text-pink-600">
-        Hi there
-    </div>
+    <Navbar />
+    <Home />
 </template>
 
