4458ac3572a63f0725893f2399e7c7a371290d8b
Odumz <odumz20@gmail.com>
2022-02-06 01:54:31 +0100

    navbar component added

---
+++ src/components/Navbar.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+</script>
+
+<template>
+    <nav class="flex justify-between bg-blue-900 items-center py-5 px-16">
+        <div class="logo flex">
+            <router-link class="flex justify-center items-center" to="/">
+                <img src="../assets/farmer.png" alt="farmer-logo">
+                <p class="px-3 text-xl text-white">GriffinFarm</p>
+            </router-link>
+        </div>
+        <div class="text-gray-400 flex">
+            <p>0x0</p>
+        </div>
+    </nav>
+</template>
